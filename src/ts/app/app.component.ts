01d30a348c363be1139a379b18ba7e00faef51d9
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2017-10-02 13:46:47 -0300

    Configuring CSS Classes to Show Validation

--- src/ts/app/app.component.ts
+++ src/ts/app/app.component.ts
@@ -2,10 +2,22 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "main",
-    template: "<span>{{message}}</span>",
+    styles: [
+        "input.ng-invalid.ng-touched { border: 2px solid red; margin: 5px; }",
+        "input + span { display: none; }",
+        "input.ng-invalid.ng-touched + span { display: inline; }",
+    ],
+    template: `
+        <form>
+            <fieldset ngModelGroup="someGroup">
+                <input type="text" id="message-input" name="messageInput" required [(ngModel)]="message">
+                <span>Message id required.</span>
+            </fieldset>
+        </form>
+    `,
 })
 export class AppComponent {
 
-    public message: string = "Hello World!";
+    public message: string = "";
 
 }
